4ecd3d369ed9f4ff998a17ea548840593b322b51
luigi vanacore <luigi.vanacore@outlook.com>
2024-07-12 01:21:33 +0200

    corretto lo script per installare kubernetes

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,25 +27,30 @@ resource "null_resource" "install_k8s_master" {
 
   provisioner "remote-exec" {
     inline = [
-      "sudo apt-get update && sudo apt-get install -y apt-transport-https curl",
-       "sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -",
-        "echo 'deb https://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee /etc/apt/sources.list.d/kubernetes.list",
-        "sudo apt update",
-        "sudo apt -y install vim git wget",
-        "sudo snap install kubectl --classic",
-        "sudo snap install kubeadm --classic",
-        "sudo snap install kubelet --classic",
-        "sudo apt-mark hold kubelet kubeadm kubectl",
-
-      "curl -LO https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.20.0/crictl-v1.20.0-linux-amd64.tar.gz",
-      "sudo tar -C /usr/local/bin -xzf crictl-v1.20.0-linux-amd64.tar.gz",
-      "sudo systemctl enable kubelet",
-      "sudo systemctl start kubelet",
-      "sudo kubeadm init --apiserver-advertise-address=${var.master_ip} --pod-network-cidr=10.244.0.0/16",
-      "mkdir -p $HOME/.kube",
-      "sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config",
-      "sudo chown $(id -u):$(id -g) $HOME/.kube/config",
-      "kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml"
+      "sudo apt-get update && sudo apt-get upgrade -y",
+      "sudo swapoff -a",
+      "sudo sed -i '/ swap / s/^/#/' /etc/fstab",
+      "echo 'overlay\nbr_netfilter' | sudo tee /etc/modules-load.d/containerd.conf",
+      "sudo modprobe overlay",
+      "sudo modprobe br_netfilter",
+      "echo 'net.bridge.bridge-nf-call-ip6tables = 1\nnet.bridge.bridge-nf-call-iptables = 1\nnet.ipv4.ip_forward = 1' | sudo tee /etc/sysctl.d/kubernetes.conf",
+      "sudo sysctl --system",
+      "sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker.gpg",
+      "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"",
+      "sudo apt update",
+      "sudo apt install -y containerd.io",
+      "containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1",
+      "sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml",
+      "sudo systemctl restart containerd",
+      "sudo systemctl enable containerd",
+      "sudo apt-get update",
+      "sudo apt-get install -y apt-transport-https ca-certificates curl gpg",
+      "curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg",
+      "echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list",
+      "sudo apt update",
+      "sudo apt install -y kubelet kubeadm kubectl",
+      "sudo apt-mark hold kubelet kubeadm kubectl"
     ]
   }
 }
@@ -57,26 +62,36 @@ resource "null_resource" "install_k8s_worker1" {
     type        = "ssh"
     host        = var.worker1_ip
     user        = "vagrant"
-    private_key = file("../servers/.vagrant/machines/worker2/virtualbox/private_key")
+    private_key = file("../servers/.vagrant/machines/worker1/virtualbox/private_key")
   }
 
   provisioner "remote-exec" {
     inline = [
-      "sudo apt-get update && sudo apt-get install -y apt-transport-https curl",
-      "sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -",
-        "echo 'deb https://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee /etc/apt/sources.list.d/kubernetes.list",
-        "sudo apt update",
-        "sudo apt -y install vim git wget",
-        "sudo snap install kubectl --classic",
-        "sudo snap install kubeadm --classic",
-        "sudo snap install kubelet --classic",
-        "sudo apt-mark hold kubelet kubeadm kubectl",
-      "curl -LO https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.20.0/crictl-v1.20.0-linux-amd64.tar.gz",
-      "sudo tar -C /usr/local/bin -xzf crictl-v1.20.0-linux-amd64.tar.gz",
-      "sudo systemctl enable kubelet",
-      "sudo systemctl start kubelet",
-      "JOIN_COMMAND=$(ssh vagrant@${var.master_ip} sudo kubeadm token create --print-join-command)",
-      "sudo $JOIN_COMMAND"
+      "sudo apt-get update && sudo apt-get upgrade -y",
+      "sudo swapoff -a",
+      "sudo sed -i '/ swap / s/^/#/' /etc/fstab",
+      "sudo sed -i \"/#\\$nrconf{restart} = 'i';/s/.*/\\$nrconf{restart} = 'a';/\" /etc/needrestart/needrestart.conf",
+      "echo 'overlay\nbr_netfilter' | sudo tee /etc/modules-load.d/containerd.conf",
+      "sudo modprobe overlay",
+      "sudo modprobe br_netfilter",
+      "echo 'net.bridge.bridge-nf-call-ip6tables = 1\nnet.bridge.bridge-nf-call-iptables = 1\nnet.ipv4.ip_forward = 1' | sudo tee /etc/sysctl.d/kubernetes.conf",
+      "sudo sysctl --system",
+      "sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker.gpg",
+      "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"",
+      "sudo apt update",
+      "sudo apt install -y containerd.io",
+      "containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1",
+      "sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml",
+      "sudo systemctl restart containerd",
+      "sudo systemctl enable containerd",
+      "sudo apt-get update",
+      "sudo apt-get install -y apt-transport-https ca-certificates curl gpg",
+      "curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg",
+      "echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list",
+      "sudo apt update",
+      "sudo apt install -y kubelet kubeadm kubectl",
+      "sudo apt-mark hold kubelet kubeadm kubectl"
     ]
   }
 }
@@ -93,21 +108,30 @@ resource "null_resource" "install_k8s_worker2" {
 
   provisioner "remote-exec" {
     inline = [
-      "sudo apt-get update && sudo apt-get install -y apt-transport-https curl",
-      "sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -",
-        "echo 'deb https://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee /etc/apt/sources.list.d/kubernetes.list",
-        "sudo apt update",
-        "sudo apt -y install vim git wget",
-        "sudo snap install kubectl --classic",
-        "sudo snap install kubeadm --classic",
-        "sudo snap install kubelet --classic",
-        "sudo apt-mark hold kubelet kubeadm kubectl",
-      "curl -LO https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.20.0/crictl-v1.20.0-linux-amd64.tar.gz",
-      "sudo tar -C /usr/local/bin -xzf crictl-v1.20.0-linux-amd64.tar.gz",
-      "sudo systemctl enable kubelet",
-      "sudo systemctl start kubelet",
-      "JOIN_COMMAND=$(ssh vagrant@${var.master_ip} sudo kubeadm token create --print-join-command)",
-      "sudo $JOIN_COMMAND"
+      "sudo apt-get update && sudo apt-get upgrade -y",
+      "sudo swapoff -a",
+      "sudo sed -i '/ swap / s/^/#/' /etc/fstab",
+      "echo 'overlay\nbr_netfilter' | sudo tee /etc/modules-load.d/containerd.conf",
+      "sudo modprobe overlay",
+      "sudo modprobe br_netfilter",
+      "echo 'net.bridge.bridge-nf-call-ip6tables = 1\nnet.bridge.bridge-nf-call-iptables = 1\nnet.ipv4.ip_forward = 1' | sudo tee /etc/sysctl.d/kubernetes.conf",
+      "sudo sysctl --system",
+      "sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker.gpg",
+      "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"",
+      "sudo apt update",
+      "sudo apt install -y containerd.io",
+      "containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1",
+      "sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml",
+      "sudo systemctl restart containerd",
+      "sudo systemctl enable containerd",
+      "sudo apt-get update",
+      "sudo apt-get install -y apt-transport-https ca-certificates curl gpg",
+      "curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg",
+      "echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list",
+      "sudo apt update",
+      "sudo apt install -y kubelet kubeadm kubectl",
+      "sudo apt-mark hold kubelet kubeadm kubectl"
     ]
   }
 }
@@ -123,4 +147,3 @@ output "worker1_ip" {
 output "worker2_ip" {
   value = var.worker2_ip
 }
-
